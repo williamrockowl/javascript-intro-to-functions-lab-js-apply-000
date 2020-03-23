4afdd47e52a55a9087bb4b28018078216d4ad4f2
salubrious-lifecycle-8559 <williamsaulon24@gmail.com>
2020-03-23 19:34:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   
-  if(string.toUpperCase() == upperCase)
+  if(string.toUpperCase() == string)
   return "YES INDEED!";
   
   
