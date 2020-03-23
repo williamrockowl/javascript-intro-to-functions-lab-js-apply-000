305e07de94acbd2d7607a6700d66f37b12a4884c
salubrious-lifecycle-8559 <williamsaulon24@gmail.com>
2020-03-23 19:36:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string){
   return "I can't hear you!";
   if(string.toUpperCase() == string)
   return "YES INDEED!";
-  
-  
+  if(string.toLowerCase() && string.toUpperCase() === string)
+  return "I love you, too."
   
 }
